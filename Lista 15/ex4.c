c2bbdd14298c4f27fdf9a6b2125038eaf2b1d2da
Gustavoleal94 <131825713+Gustavoleal94@users.noreply.github.com>
2023-04-25 22:19:54 -0300

    ex4
    ex4

---
+++ Lista 15/ex4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main (){
+	int num1;
+	int num2;
+	int soma;
+
+	printf("informe um numero: ");
+	scanf("%d", &num1);
+	printf("informe outro numero: ");
+	scanf("%d", &num2);
+	soma = num1 + num2;
+	printf("a soma dos numeros eh: %d ", soma);
+
+	
+	
+}
