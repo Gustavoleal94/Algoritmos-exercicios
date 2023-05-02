72b1644e11f0e189fe8f20238bc625006b5c60bc
Gustavoleal94 <131825713+Gustavoleal94@users.noreply.github.com>
2023-05-02 19:38:46 -0300

    ex5
    ex5

---
+++ Lista 15/ex5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main (){
+	int num1, num2, num3, num4;
+	int mult;
+
+	printf("informe primeiro numero: ");
+	scanf("%d", &num1);
+	printf("informe segundo numero: ");
+	scanf("%d", &num2);
+	printf("informe terceiro numero: ");
+	scanf("%d", &num3);
+	printf("informe quarto numero: ");
+	scanf("%d", &num4);
+	mult = num1 * num2 * num3 * num4;
+	printf("a multiplicacao dos numeros = : %d ", mult);
+
+	
+	
+}
